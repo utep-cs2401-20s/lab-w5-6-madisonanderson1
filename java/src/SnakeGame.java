c1a7e7472774255a05df1ff8cdb06938faea47a4
madia <madisonanderson1@gmail.com>
2020-02-17 14:31:52 -0700

    Start the constructors

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,3 +1,21 @@
 public class SnakeGame {
+    private boolean[][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    public SnakeGame (){
+        game = new boolean[][]{{false}};
+    }
+
+    public SnakeGame(boolean[][] array, int x, int y){
+        for (int i = 0; i<array.length; i++){
+            for (int j = 0; j<array[0].length; j++){
+                game[i][j] = array[i][j];
+            }
+        }
+        int[] headPosition = {x,y};
+    }
+
     
 }
